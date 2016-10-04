338782243f5509631c1b874dfd9cefad4d6ae309
Ralph Schmieder <rschmied@cisco.com>
2016-10-04 19:07:43 +0200

    Squashed commit of the following:
    commit b409f3625e320f6838cf0eb54359ae56b329d04f
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Tue Oct 4 19:07:15 2016 +0200
    
        postman collection and environment updates
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit 4f42cb38a3b7198d0797e319140ded668275995f
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Tue Oct 4 19:06:50 2016 +0200
    
        shuffle around
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit 934b86e2bc182412e5b77769d2dc45948dabdcb6
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Tue Oct 4 19:02:06 2016 +0200
    
        module 04 rename
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit cc403f9c3a2c9fa855bc49d81baccdf73c778762
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Tue Oct 4 19:01:09 2016 +0200
    
        module 4 rename
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit 470fe92e3450a7b4cccebb90a34782aa680f60cb
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Tue Oct 4 19:00:48 2016 +0200
    
        module 2 rename
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit c292f643238d6c22db0296c0306f95722333158a
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Tue Oct 4 19:00:01 2016 +0200
    
        module01 rename
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- module06/get_ietf_interfaces.py
+++ module06/get_ietf_interfaces.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
-import logging
-from lxml import etree
 from ncclient import manager
-from ncclient.xml_ import *
 import sys
 import xml.dom.minidom
 
 # the variables below assume the user is leveraging the
 # network programmability lab and accessing csr1000v
 # use the IP address or hostname of your CSR1000V device
-HOST = 'ios-xe-mgmt.cisco.com'
+HOST = '198.18.133.218'
 # use the NETCONF port for your CSR1000V device
-PORT = 10000
+PORT = 2022
 # use the user credentials for your CSR1000V device
-USER = 'root'
-PASS = 'C!sc0123'
+USER = 'admin'
+PASS = 'C1sco12345'
 MODULE_NAME = 'ietf-interfaces.yang'
 
 
 # create a main() method
 def main():
-    """Main method that prints netconf capabilities of remote device."""
-    with manager.connect(host=HOST, port=PORT, username=USER, password=PASS,
-                         hostkey_verify=False, device_params={'name': 'default'},
+    """
+    Main method that prints netconf capabilities of remote device.
+    """
+    with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
                          look_for_keys=False, allow_agent=False) as m:
 
         # print YANG module
--- module06/screen_scraping-2.py
+++ module06/screen_scraping-2.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import re
 
 
 def main():
-    """Open a file called static-routes.txt and print each line that matches a REGEX for a static route."""
+    """
+    Open a file called static-routes.txt and print each line that
+    matches a REGEX for a static route.
+    """
     with open('static-routes.txt', 'r') as static_routes:
         for route in static_routes:
             if re.match('^ip route (\d{1,3}\.){3}\d{1,3} '
