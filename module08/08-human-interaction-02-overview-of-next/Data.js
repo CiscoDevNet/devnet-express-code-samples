f0023d4f43b1a45eb1eeaeb682dc0d87d22333b3
darien-hirotsu <darien@sdnessentials.com>
2016-10-07 10:48:28 -0700

    Module 08 code examples

---
+++ module08/08-human-interaction-02-overview-of-next/Data.js
@@ -0,0 +1,9 @@
+var topologyData = {
+    nodes: [
+        {"id": 0, "x": 410, "y": 100, "name": "router 1"},
+        {"id": 1, "x": 660, "y": 100, "name": "router 2"}
+    ],
+    links: [
+        {"source": 0, "target": 1}
+    ]
+};
