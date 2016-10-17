21da5aa19a842eba0e39c48953427e4ed77ccd16
brtiller <brtiller@cisco.com>
2016-10-17 17:27:25 -0400

    mod 4 new python script

--- module08/08-lab03-mission/human_interaction_mission.py
+++ module08/08-lab03-mission/human_interaction_mission.py
@@ -49,12 +49,12 @@ def get_tropo_app_id(tropo_api,app_name, tropo_user, tropo_pass):
 def get_tropo_phone_number(tropo_api, tropo_app_id, tropo_user, tropo_pass):
 	phone_num=None
 	# Content type must be included in the header
-	header = {"content-type": "application/json"}
+	#header = {"content-type": "application/json"}
 	# Create the URI string for our Tropo application
 	url = "{api}{address}{app}{addr}".format(api=tropo_api,
 											address="/applications/",
 											app=tropo_app_id,
-											addr="/addresses")	
+											addr="/addresses")		
 	response = requests.get(url,auth=(tropo_user,tropo_pass),headers=header,verify=False)
 	if(response):				
 		resp=response.json()
