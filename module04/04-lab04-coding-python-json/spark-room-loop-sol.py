94752ec65114965475f4482321056b69241210ac
brtiller <brtiller@cisco.com>
2017-01-11 17:56:26 -0500

    module 4 scripts

---
+++ module04/04-lab04-coding-python-json/spark-room-loop-sol.py
@@ -0,0 +1,31 @@
+import json
+import requests
+
+accessToken = "" #put your access token here between the quotes.
+
+
+def setHeaders():         
+	accessToken_hdr = 'Bearer ' + accessToken
+	spark_header = {'Authorization': accessToken_hdr, 'Content-Type': 'application/json; charset=utf-8'}
+	return spark_header
+
+
+def getRooms(theHeader):    
+	uri = 'https://api.ciscospark.com/v1/rooms'
+	resp = requests.get(uri, headers=theHeader)	
+	return resp.json()
+
+def parseData(theData):
+	for data in theData["items"]:	
+		for room_info in data:
+			key = room_info
+			value = str(data[room_info])
+			print(key + ":  " + value)
+		print()
+		print()
+
+header=setHeaders()
+value=getRooms(header)
+print ("Spark Response Data:")
+print (json.dumps(value, indent=4, separators=(',', ': ')))
+parseData(value)
