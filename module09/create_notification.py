0ff5446868fe6f0f1f89827269673557e885740a
darien-hirotsu <darien@sdnessentials.com>
2017-01-18 12:01:28 -0800

    Updates for CMX mission

--- module09/create_notification.py
+++ module09/create_notification.py
@@ -5,29 +5,30 @@
 # darien@sdnessentials.com
 #
 
-import json
+
 import requests
 from requests.auth import HTTPBasicAuth
 import sys
 requests.packages.urllib3.disable_warnings()
 
+USERNAME = 'learning'
+PASSWORD = 'learning'
+URI = 'https://msesandbox.cisco.com:8081/api/config/v1/notification'
+PAYLOAD = 'notification.json'
+
 
 def main():
     """Simple main method to create a CMX notification."""
     try:
-        username = 'learning'
-        password = 'learning'
-        f = open('notification.json', 'r')
+        f = open(PAYLOAD, 'r')
         data = f.read().replace('\n', '')
-        uri = 'https://msesandbox.cisco.com:8081/api/config/v1/notification'
-        print(type(data))
-        print(json.dumps(data))
+        # print(json.dumps(data))
         headers = {
             'content-type': "application/json",
             'accept': "application/json"
         }
-        request = requests.put(url=uri,
-                               auth=HTTPBasicAuth(username, password),
+        request = requests.put(url=URI,
+                               auth=HTTPBasicAuth(USERNAME, PASSWORD),
                                data=data,
                                headers=headers,
                                verify=False)
