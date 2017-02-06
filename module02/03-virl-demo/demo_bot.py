9652b15edc2a51619f60f5e526b444b41912581a
Armen Martirosyan <armen@sdnessentials.com>
2017-02-06 12:52:51 -0800

    py script for VIRL demo

---
+++ module02/03-virl-demo/demo_bot.py
@@ -0,0 +1,369 @@
+#!/usr/bin/python3
+
+# Import necessary modules
+from pprint import pprint
+import requests
+import json
+import sys
+# Attempt to import flask module
+try:
+    from flask import Flask
+    from flask import request
+# If fails attempt to install the module
+except:
+    try:
+        import pip
+        pip.main(['install', "flask"])
+        from flask import Flask
+        from flask import request
+        print("Flask was successfully imported")
+    except PermissionError as e:
+        print("You don't have permissions to install flask library.\n"
+              "Try to run the script with elevated privileges.")
+        print(e)
+        exit()
+import subprocess
+import time
+import os
+from virl import start_sim
+from virl import stop_sim
+import platform
+import zipfile
+import logging
+
+## Define logging parameters
+#log = logging.getLogger('VIRL')
+## change this to logging.ERROR for silence
+#log.setLevel(logging.DEBUG)
+#FORMAT = '%(asctime)s %(funcName)s(): %(message)s'
+#logging.basicConfig(format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
+
+# Define global parameters
+print('Setting global parameters.')
+URL = "https://api.ciscospark.com/v1"
+# BOT'S ACCESS TOKEN
+bearer = "Zjk4Yjk0NWItZDZlMC00ZTJlLWIzZDYtMTA3YzBkMTc5MDBiOGNmNjNkNGQtNDI0"
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Bearer " + bearer
+}
+
+
+def ngrok():
+    num = 0
+    if platform.system() == "Windows":
+        num+=1
+        print(num)
+        print("Downloading ngrok for Windows OS from website...")
+        win_ngrok = requests.get(
+            "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip")
+        print("Success!")
+        print("Unzipping ngrok.zip file....")
+        with open("ngrok.zip", "wb") as f:
+            f.write(win_ngrok.content)
+        with zipfile.ZipFile("ngrok.zip", "r") as z:
+            z.extractall("./")
+            print("Success!")
+        return True
+    else:
+        print("Checking if ngrok is present in UNIX PATH folder...")
+        check_ngrok = subprocess.check_output(
+            ["ls -al /home/cisco/Code/mycode/bin | grep ngrok"], shell=True).decode("utf-8")
+        if "x" in check_ngrok:
+            print("Success!")
+            return True
+        else:
+            print("ngrok was not found.")
+            print("Downloading ngrok for UNIX OS from website...")
+            set_ngrok = subprocess.Popen(
+                ["wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip && mv ngrok /home/cisco/Code/mycode/bin"], shell=True)
+            while set_ngrok.poll() is None:
+                continue
+            check_ngrok = subprocess.check_output(
+                ["ls -al /home/cisco/Code/mycode/bin | grep ngrok"], shell=True).decode("utf-8")
+            if "x" in check_ngrok:
+                print(
+                    "ngrok was successfully downloaded and moved to PATH folder")
+                return True
+            else:
+                print("Something went wrong!")
+                return False
+
+
+def send_spark_get(url, payload=None, js=True):
+
+    if payload == None:
+        request = requests.get(url, headers=headers)
+    else:
+        request = requests.get(url, headers=headers, params=payload)
+    if js == True:
+        request = request.json()
+    return request
+
+
+def send_spark_post(url, data, js=True):
+
+    request = requests.post(url, json.dumps(data), headers=headers)
+    if js:
+        request = request.json()
+    return request
+
+
+def send_spark_delete(url, js=False):
+
+    request = requests.post(url, headers=headers)
+    if js != False:
+        request = request.json()
+    return request
+
+
+def send_spark_put(url, data, js=False):
+
+    request = requests.put(url, data=json.dumps(data), headers=headers)
+    if js:
+        request = request.json()
+    return request
+
+
+def help_me():
+
+    return "Sure! I can help. Below are the commands that I understand:<br/>" \
+        "`Run [attached file]` - I will run attached virl file.<br/>" \
+        "`Start [simulation file name]` - I will start specified simulation<br/>" \
+        "`Stop [simulation id]` - I will stop simulation with specified ID<br/>" \
+        "`List` - I will provide a list of VIRL files present on local HDD<br/>" \
+        "`Check VIRL` - I will check VIRL and see if any simulations are running<br/>"
+
+
+def greetings():
+
+    return "Hi my name is %s.<br/>" \
+        "Type `Help me` to see what I can do.<br/>" % bot_name
+
+
+def handle_text(text, filename=None):
+    result = None
+    if text.lower().startswith('hello'):
+        result = greetings()
+    if text.lower().startswith('help me'):
+        result = help_me()
+    if text.lower().startswith('run') and not filename:
+        result = "Please don't forget to upload a VIRL file with your `run` command"
+    elif text.lower().startswith('run') and not filename.endswith(".virl"):
+        result = "I can't run a non VIRL file. Type `Help Me` to see what I can do"
+    elif text.lower().startswith('run') and filename.endswith(".virl"):
+        sim_id = start_sim(filename)
+        if sim_id != -1:
+            result = "Simulation %s was successfully started " % sim_id
+        else:
+            result = "Sorry, but I was not able to start the simulation!"
+    if text.lower().startswith('stop') and len(text) > 5:
+        response = stop_sim(text.split(" ")[1])
+        if response:
+            result = "The simulation was successfully stopped!"
+        else:
+            result = "Something went wrong while I was trying to stop a simulation.\n" \
+                "Please make sure provided simulation ID is correct!"
+    if text.lower().startswith('start') and len(text) > 6:
+        response = start_sim(text.split(" ")[1])
+        if response != False:
+            result = "Simulation `" + str(response) + "` was successfully started."
+        else:
+            result = "Something went wrong while I was trying to start the simulation.\n" \
+                "Please make sure provided VIRL file exist on local HDD!"
+    if text.lower().startswith('list'):
+        result = virl_files()
+    if result == None:
+        result = "I did not understand your request. Please type `Help me` to see what I can do"
+    return result
+
+
+def get_files(file_urls, room_id):
+
+    for file_url in file_urls:
+        response = send_spark_get(file_url, js=False)
+        content_disp = response.headers.get('Content-Disposition', None)
+        if content_disp is not None:
+            filename = content_disp.split("filename=")[1].replace('"', '')
+        if filename.endswith('.virl'):
+            with open("./sims/" + filename, 'wb') as f:
+                f.write(response.content)
+                send_spark_post(URL + "/messages",
+                                {"roomId": room_id, "markdown": ' Received and saved - ' + filename})
+                return filename
+        else:
+            send_spark_post(URL + "/messages",
+                            {"roomId": room_id, "markdown": '**Sorry but I only accept VIRL files**'})
+
+
+def webhook():
+
+    url = URL + "/webhooks"
+    webhooks = {}
+    resources = ["messages", "memberships"]
+    webhook_name = "Webhook for demo"
+    event = "created"
+    status = None
+    name = "Webhook Demo"
+
+    ngrok_url = requests.get(
+        "http://127.0.0.1:4040/api/tunnels", headers={"Content-Type": "application/json"}).json()
+    for urls in ngrok_url["tunnels"]:
+        if "https://" in urls['public_url']:
+            target_url = urls['public_url']
+
+    check_webhook = send_spark_get(url, js=False)
+    if check_webhook.ok:
+        check_webhook = check_webhook.json()
+        if len(check_webhook["items"]) > 0:
+            for items in check_webhook["items"]:
+                webhooks[items["resource"]] = items["id"]
+    if len(webhooks) == 0:
+        for resource in resources:
+            payload = {"name": name, "targetUrl": target_url,
+                       "resource": resource, "event": "created"}
+            webhook = send_spark_post(url, payload, js=False)
+            if webhook.ok:
+                status = True
+                print("Webhook was successfully created")
+            else:
+                status = False
+                print(
+                    "Something went wrong. I was unable to create the webhook")
+    else:
+        for resource in resources:
+            payload = {"name": name, "targetUrl": target_url}
+            webhook = send_spark_put(
+                url + "/" + webhooks[resource], payload, js=False)
+            if webhook.ok:
+                status = True
+                print("Webhook was successfully updated")
+            else:
+                status = False
+                print(
+                    "Something went wrong. I was unable to update the webhook")
+
+    return status
+
+
+def virl_files():
+
+    contents = ""
+    num = 0
+    file_name = None
+    for file in os.listdir("./sims/"):
+        if file.endswith(".virl"):
+            num += 1
+            contents += str(num) + ". "*5 + str(file) + "<br/>"
+            if file_name == None:
+                file_name = file
+    if num == 0:
+        return "**No VIRL files are present on local HDD.<br/>" \
+            "Use `run` command to upload a file and run it."
+    return "**Found " + str(num) + " VIRL file(s) on local HDD**.<br/>" + contents + \
+        "**Note:** You can say `start " + file_name + \
+        "` and I will start the simulation for you."
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def spark_webhook():
+    if request.method == 'POST':
+        webhook = request.get_json(silent=True)
+        resource = webhook['resource']
+        senders_email = webhook['data']['personEmail']
+        room_id = webhook['data']['roomId']
+
+        if senders_email != bot_email:
+            pprint(webhook)
+        if resource == "memberships" and senders_email == bot_email:
+            send_spark_post(URL + "/messages",
+                            {
+                                "roomId": room_id,
+                                "markdown": (greetings() +
+                                             "**Note This is a group room and you have to call "
+                                             "me specifically with `@%s` for me to respond**" % bot_name)
+                            }
+                            )
+
+        msg = None
+        filename = None
+        if senders_email != bot_email:
+            if "files" in webhook['data']:
+                filename = get_files(webhook['data']['files'], room_id)
+            result = send_spark_get(
+                URL + '/messages/{0}'.format(webhook['data']['id']))
+            in_message = result.get('text', '')
+            print("Received " + in_message + " from spark. Processing...")
+            in_message = in_message.replace(bot_name.lower() + " ", '')
+            if filename != None:
+                msg = handle_text(in_message, filename=filename)
+            else:
+                msg = handle_text(in_message)
+            if msg != None:
+                send_spark_post(URL + "/messages",
+                                {"roomId": room_id, "markdown": msg})
+        return "true"
+    elif request.method == 'GET':
+        message = "<center><img src=\"http://bit.ly/SparkBot-512x512\" alt=\"Spark Bot\" style=\"width:256; height:256;\"</center>" \
+            "<center><h2><b>Congratulations! Your <i style=\"color:#ff8000;\">%s</i> bot is up and running.</b></h2></center>" \
+            "<center><b><i>Please don't forget to create Webhooks to start receiving events from Cisco Spark!</i></b></center>" % bot_name
+        return message
+
+
+def main():
+    global bot_email, bot_name
+    if len(bearer) != 0:
+        test_auth = send_spark_get(URL + "/people/me", js=False)
+        if test_auth.status_code == 401:
+            print("Looks like provided access token is not correct. \n"
+                  "Please review it and make sure it belongs to your bot account.\n"
+                  "Do not worry if you have lost the access token. "
+                  "You can always go to https://developer.ciscospark.com/apps.html "
+                  "URL and generate a new access token.")
+            sys.exit()
+        if test_auth.status_code == 200:
+            test_auth = test_auth.json()
+            bot_name = test_auth.get("displayName", "")
+            bot_email = test_auth.get("emails", "")[0]
+    else:
+        print("'bearer' variable is empty! \n"
+              "Please populate it with bot's access token and run the script again.\n"
+              "Do not worry if you have lost the access token. "
+              "You can always go to https://developer.ciscospark.com/apps.html "
+              "URL and generate a new access token.")
+        sys.exit()
+
+    if "@sparkbot.io" not in bot_email:
+        print("You have provided access token which does not belong to your bot.\n"
+              "Please review it and make sure it belongs to your bot account.\n"
+              "Do not worry if you have lost the access token. "
+              "You can always go to https://developer.ciscospark.com/apps.html "
+              "URL and generate a new access token.")
+        sys.exit()
+    else:
+        app.run(host='localhost', port=8080)
+
+if __name__ == "__main__":
+    if ngrok():
+        if platform.system() != "Windows":
+            ngrok_run = subprocess.Popen(
+                ["ngrok http 8080"], shell=True,
+                 stdout=subprocess.PIPE,
+                  stderr=subprocess.PIPE,
+                   universal_newlines=True)
+            print("Waiting for ngrok to come up ...")
+            time.sleep(2)
+            print("Success! Ngrok is up")
+            if webhook():
+                main()
+        elif platform.system() == "Windows":
+            ngrok_run = os.popen("ngrok http 8080", mode='r', buffering=-1)
+            print("Waiting for ngrok to come up ...")
+            time.sleep(2)
+            print("Success! Ngrok is up")
+            if webhook():
+                main()
