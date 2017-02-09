172e5f3d7344ad5d18baba5d3342591cfd164659
brtiller <brtiller@cisco.com>
2017-02-09 16:58:47 -0500

    mod 4 mission update

--- module04/04-lab05-mission/spark_mission.py
+++ module04/04-lab05-mission/spark_mission.py
@@ -14,22 +14,42 @@ def setHeaders():
 	spark_header = {'Authorization': accessToken_hdr, 'Content-Type': 'application/json; charset=utf-8'}
 	return (spark_header)
 
-	
-# creates a new room and returns the room id.
-def createRoom(the_header,room_name):
-	roomInfo = {"title":room_name}
+
+#check if spark room already exists.  If so return the room id
+def findRoom(the_header,room_name):
+	roomId=None
 	uri = 'https://api.ciscospark.com/v1/rooms'
-	resp = requests.post(uri, json=roomInfo, headers=the_header)
-	var = resp.json()
-	print("createRoom JSON: ", var)	
-	#MISSION: REPLACE None WITH CODE THAT PARSES AND RETURNS THE ROOM ID.	
-	return(None)
+	resp = requests.get(uri, headers=the_header)
+	resp = resp.json()
+	for room in resp["items"]:
+		if room["title"] == room_name:
+			print()
+			print("findRoom JSON: ", room)	
+			print("MISSION: findRoom: REPLACE None WITH CODE THAT PARSES JSON TO ASSIGN ROOM ID VALUE TO VARIABLE roomId")
+			roomId=None
+			break	
+	return(roomId)
+	
+# checks if room already exists and if true returns that room ID. If not creates a new room and returns the room id.
+def createRoom(the_header,room_name):
+	roomId=findRoom(the_header,room_name)
+	if roomId==None:
+		roomInfo = {"title":room_name}
+		uri = 'https://api.ciscospark.com/v1/rooms'
+		resp = requests.post(uri, json=roomInfo, headers=the_header)
+		var = resp.json()		
+		print()
+		print("createRoom JSON: ", var)	
+		print("MISSION: createRoom: REPLACE None WITH CODE THAT PARSES JSON TO ASSIGN ROOM ID VALUE TO VARIABLE roomId.")		
+		roomId=None		
+	return(roomId)
 	
 # adds a new member to the room.  Member e-mail is test@test.com
 def addMembers(the_header,roomId):
 	member = {"roomId":roomId,"personEmail": "test@test.com", "isModerator": False}
 	uri = 'https://api.ciscospark.com/v1/memberships'
 	resp = requests.post(uri, json=member, headers=the_header)
+	print()
 	print("addMembers JSON: ", resp.json())
 
 #posts a message to the room
@@ -37,6 +57,7 @@ def postMsg(the_header,roomId,message):
 	message = {"roomId":roomId,"text":message}
 	uri = 'https://api.ciscospark.com/v1/messages'
 	resp = requests.post(uri, json=message, headers=the_header)
+	print()
 	print("postMsg JSON: ", resp.json())
 
 #MISSION: WRITE CODE TO RETRIEVE AND DISPLAY DETAILS ABOUT THE ROOM.
@@ -49,7 +70,7 @@ def getRoomInfo(the_header,roomId):
 	resp = requests.get(uri, headers=the_header)
 	print("Room Info: ",resp.text)
 	resp = resp.json()
-	print("MISSION: Please add code to parse and display details about the room.")
+	print("MISSION: Add code to parse and display details about the room.")
 
 
 if __name__ == '__main__':
@@ -59,10 +80,10 @@ if __name__ == '__main__':
 	#passing the ROOM_NAME for the room to be created
 	room_id=createRoom(header,ROOM_NAME) 
 	if room_id == None:
-		sys.exit("Please check that function createRoom returns the room ID value.")
+		sys.exit("Please check that functions findRoom and createRoom return the room ID value.")
 	#passing roomId to members function here to add member to the room.
 	addMembers(header,room_id)   
 	#passing roomId to message function here to Post Message to a room.
 	postMsg(header,room_id,YOUR_MESSAGE)
-	#MISSION: ADD FUNCTION CALL getRoomInfo(header,room_id)
+	print()
 	print("MISSION: ADD FUNCTION CALL getRoomInfo(header,room_id)")
