470c9d7760e7ac903b2a607dab0bd21ba73ce3f7
brtiller <brtiller@cisco.com>
2017-03-20 18:09:14 -0400

    Concat file fixes

--- module00/00-prep-03-python-primer/concat.py
+++ module00/00-prep-03-python-primer/concat.py
@@ -2,10 +2,10 @@ myVarRed= "Red"
 myVarBlue= "Blue"
 
 print("Roses are Red. " + "Violets are Blue.")
-print("Roses are" + " " + myVarRed + ". Violets are " + myVarBlue)
+print("Roses are " + myVarRed + ". Violets are " + myVarBlue)
 
 myStr = "Roses are Red. " + "Violets are Blue."
-varStr = "Roses are" + " " + myVarRed + ". Violets are " + myVarBlue
+varStr = "Roses are " + myVarRed + ". Violets are " + myVarBlue
 
 print(myStr)
 print(varStr)
