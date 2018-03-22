ed43f97d3514d167d308ee69e12fe767aae26b15
Chris Lunsford <chrlunsf@cisco.com>
2018-03-21 18:56:57 -0600

    Initial condiitionals Script
    Hands-on exercise:
    * Using print statements with string formatting and/or string concatenation
    * Inspecting Python data types
    * Writing conditionals to inspect variables and control flow

---
+++ LM-1201/99-beta/conditionals.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Basic data types and conditionals."""
+
+
+import math
+import random
+
+
+# TODO: Write a print statement that displays both the type and value of `pi`
+pi = math.pi
+
+
+# TODO: Write a conditional to print out if `i` is less than or greater than 50
+i = random.randint(0, 100)
+
+
+# TODO: Write a conditional that prints the color of the picked fruit
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
